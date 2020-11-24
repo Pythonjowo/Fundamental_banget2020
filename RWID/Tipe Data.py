add88976c033ebc23b2ced6e3ae76e10af9cbb26
psfauzi <psfauzi@gmail.com>
2020-11-24 19:45:10 +0700

    RWID

--- RWID/Tipe Data.py
+++ RWID/Tipe Data.py
@@ -24,4 +24,30 @@ for a in  anak:
 
 print('\nSapa Semua anak: Cara Ribet')
 for a in range(0,len(anak)) :
-    print(f'Hai anak {anak[1]}')
+    print(f'{a+1}.Hai anak {anak[1]}')
+
+"""
+Tipe data dictionary sekedar menghubungkan anatara KEY dan VALUE
+KVP = Key Value Pair
+Dictionary = kamus 
+"""
+print('\n')
+kamus_ind_eng = {'anak': 'son', 'istri': 'wife', 'ayah': 'father', 'ibu': 'mother'}
+
+print(kamus_ind_eng)
+print(kamus_ind_eng['ayah'])
+print(kamus_ind_eng['ibu'])
+
+print('\n Data ini dikirimkan oleh server gojek , untuk memberikan info di sekitar pemakai aplikasi')
+data_dari_server_gojek = {
+    'tanggal' : '2020-06-10',
+    'driver_list' : [{'Nama' : 'EKo', 'Jarak' : 10}, {'Nama' : 'Dwi', 'jarak' : 30},{'Nama' :'Tri', 'jarak' : 100}, {'Nama' : 'Catur', 'jarak':'1000'}]
+
+}
+print(data_dari_server_gojek)
+print(f'Driver disekitar sini {data_dari_server_gojek["driver_list"]}')
+print(f'Driver Pertama {data_dari_server_gojek["driver_list"][0]}')
+print(f'Driver Kedua{data_dari_server_gojek["driver_list"][1]}')
+print('Jarak Driver #1')
+print(f'Driver dengan jarak terdekat {data_dari_server_gojek["driver_list"][0]["Jarak"]}m')
+
