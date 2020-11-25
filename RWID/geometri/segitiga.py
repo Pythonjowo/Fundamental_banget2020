879a1c36ec5d98f7ea2e0f3ea2c54ce1da3ba3e3
psfauzi <psfauzi@gmail.com>
2020-11-25 20:04:21 +0700

    RWID

---
+++ RWID/geometri/segitiga.py
@@ -0,0 +1,7 @@
+def info_1():
+    return 'Modul menghitung rumus rumus tentang segitiga'
+
+
+def hitug_luas_segitiga(alas, tinggi):
+    luas_segetiga = alas * tinggi / 2
+    return luas_segetiga
---
+++ RWID/geometri/use_geometri.py
@@ -0,0 +1,12 @@
+from RWID.geometri.persegipanjang import hitung_luas_persegi_panjang, info, spasi
+from RWID.geometri.segitiga import hitug_luas_segitiga, info_1
+
+# import geometri .segitiga as gs
+
+print(info())
+print(f'hitug_luas_segitiga {hitug_luas_segitiga(12,10)}')
+print(f'hitug_luas_segitiga {hitung_luas_persegi_panjang(12,10)}')
+
+print(spasi())
+print(info_1())
+print(f'Hitung Luas Persegi Panjang {hitug_luas_segitiga(11,12)}')
