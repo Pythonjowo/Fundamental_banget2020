25f05d9ecf1eb179083160881dd4ac7766feee8d
psfauzi <psfauzi@gmail.com>
2020-11-29 17:41:09 +0700

    Sekolah Koding Dasar Series

---
+++ SekolahKoding/List.py
@@ -0,0 +1,24 @@
+orang = ['krena', 'sapuro', 'dali']
+umur = [10,30,50,100,20]
+mixed = [1, 'Text','editor',2.5]
+
+"""
+index mulai dari 0.1.2.3
+
+"""
+##Menambahkan
+orang.append('Thomsan')
+print(orang[1])
+print(orang)
+
+##Mengganti List
+orang[1] = 'Ganteng'
+print(orang[1])
+
+## Menghapus List
+del orang[1]
+print(orang)
+
+
+
+
---
+++ SekolahKoding/Tuples.py
@@ -0,0 +1,13 @@
+""""
+Sequences
+List = Tuples
+"""
+
+orang = ('krena','Saputro','Dali')
+
+print(len(orang[1]))
+print(min(orang))
+print(max(orang))
+print(list(orang))
+print(tuple(orang))
+
